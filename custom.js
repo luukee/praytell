c4d286d92aee35aa52b367fcbeacfbfbb4a0218b
Luke Hartman <carl.hartman11@gmail.com>
2019-08-05 17:30:46 -0700

    --- Auto-Git Commit ---

--- custom.js
+++ custom.js
@@ -18,7 +18,7 @@
 
     // add onclick="openLightbox();" to wine bottle
     var wineBottle = document.querySelector("#wine .row .sqs-col-3:nth-child(1) .image-block");
-    wineBottles.setAttribute("onclick", "openLightbox();");
+    wineBottle.setAttribute("onclick", "openLightbox();");
 
     // change product quickview button text
     elements = document.getElementsByClassName("sqs-product-quick-view-button");
