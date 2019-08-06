735615812fe9f04dd0969f4a4d32cb9428442d17
Luke Hartman <carl.hartman11@gmail.com>
2019-08-06 07:31:31 -0700

    --- Auto-Git Commit ---

--- custom.js
+++ custom.js
@@ -42,13 +42,7 @@
     //     elem.innerHTML = 'More info. . .';
     // }
 
-    /* lightbox! */
-    // let slideIndex = 1;
-    // showSlide(slideIndex);
-
-    // You are providing the functionality for your clickable content, which is 
-    // your previews, dots, controls and the close button.
-
+    /* lightboxes! */
     function openLightbox() {
         document.getElementById('Lightbox').setAttribute("style", "display:flex; opacity:1;", "class", "active");
     }
@@ -61,38 +55,5 @@
         document.getElementById('Lightbox').setAttribute("style", "display:none; opacity:0;");
     }
 
-    // Note that you are assigning new values here to our slidIndex.
 
-    // function changeSlide(n) {
-    //     showSlide(slideIndex += n);
-    // };
-
-    // function toSlide(n) {
-    //     showSlide(slideIndex = n);
-    // };
-
-    // This is your logic for the light box. It will decide which slide to show 
-    // and which dot is active.
-
-    // function showSlide(n) {
-    //     const slides = document.getElementsByClassName('slide');
-    //     let modalPreviews = document.getElementsByClassName('modal-preview');
-
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     };
-
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     };
-
-    //     for (let i = 0; i < slides.length; i++) {
-    //         slides[i].setAttribute("style", "display:none; opacity:0;");
-    //     };
-
-    //     for (let i = 0; i < modalPreviews.length; i++) {
-    //         modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-    //     };
-
-    // };
 
