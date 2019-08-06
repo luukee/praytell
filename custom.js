7f00ead216c78fa8571b37784b8ea26206287bc1
Luke Hartman <carl.hartman11@gmail.com>
2019-08-06 10:48:01 -0700

    --- Auto-Git Commit ---

--- custom.js
+++ custom.js
@@ -7,39 +7,46 @@
 if (document.querySelector("body:not(.homepage)")) {
 
     // wine bottle image
-    var wineBottle = document.querySelector("#wine-purchase .row .sqs-col-3:nth-child(1) .thumb-image");
+    const wineBottle = document.querySelector("#wine-purchase .row .sqs-col-3:nth-child(1) .thumb-image");
+    const headerElement = document.getElementsByClassName("Header-branding")[0];
+    const mobileHeaderElement = document.getElementsByClassName("Mobile-bar-branding")[0];
+    const wineTradeContent = document.getElementById("wine-trade");
+    const wineTradeLightbox = document.querySelector("#trade-Lightbox .modal-content .close");
+    const addToCartBtn = document.querySelector(".sqs-add-to-cart-button-wrapper");
+    const lightboxBtnArea = document.querySelector("#Lightbox .sqs-block-content");
 
-    /*change header logo link if NOT on homepage */
-    var headerElement = document.getElementsByClassName("Header-branding")[0];
-    var mobileHeaderElement = document.getElementsByClassName("Mobile-bar-branding")[0];
-
-    // desktop
+    // desktop header logo
     headerElement.setAttribute("href", "/wine-home");
-    // mobile
+
+    // mobile header logo
     mobileHeaderElement.setAttribute("href", "/wine-home");
+
     // wine bottle lightbox button
     wineBottle.setAttribute("onclick", "openLightbox();");
 
     // HOMEPAGE move trade content to .homepage lightbox
-    const wineTradeContent = document.getElementById("wine-trade");
-    const wineTradeLightbox = document.querySelector("#trade-Lightbox .modal-content .close");
     wineTradeLightbox.after(wineTradeContent);
 
-    // NOT HOME move addtocart button to the .homepage lightbox
-    const addToCartBtn = document.querySelector(".sqs-add-to-cart-button-wrapper");
-    const lightboxBtnArea = document.querySelector("#Lightbox .sqs-block-content");
+    // move addtocart button to the .homepage lightbox
     addToCartBtn.after(lightboxBtnArea);
 
-    // homepage
-} else {
-    // HOMEPAGE move trade content to .homepage lightbox
+
+} else { // HOMEPAGE
+    
+    // vars
+    const wineBottle = document.querySelector("#wine .row .sqs-col-3:nth-child(1) .thumb-image");
     const tradeContent = document.getElementById("trade");
     const tradeLightbox = document.querySelector("#trade-Lightbox .modal-content .close");
-    tradeLightbox.after(tradeContent);
-
-    // move lightbox button to the .homepage lightbox
     const wineContent = document.getElementById("container-ligtbox-register-btn");
     const wineLightbox = document.getElementById("lightbox-register-button");
+
+    // HOMEPAGE move trade content to .homepage lightbox
+    tradeLightbox.after(tradeContent);
+    
+    // add onclick="openLightbox();" to wine bottle
+    wineBottle.setAttribute("onclick", "openLightbox();");
+
+    // move lightbox button to the .homepage lightbox
     wineContent.after(wineLightbox);
 }
 
@@ -50,9 +57,7 @@ if (document.querySelector("body:not(.homepage)")) {
 
 
 
-    // add onclick="openLightbox();" to wine bottle
-    var wineBottle = document.querySelector("#wine .row .sqs-col-3:nth-child(1) .thumb-image");
-    wineBottle.setAttribute("onclick", "openLightbox();");
+
 
     /* lightboxes! */
     function openLightbox() {
